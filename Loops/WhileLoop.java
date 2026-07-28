02701f279543f6057a1b21c3bda055d4154d02d5
MDTech06 <mmahekdudhediya@gmail.com>
2026-07-28 15:46:17 +0530

    Add loop folder with while file

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,13 @@
+package Loops;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+        
+        System.out.println("The loop has ended");
+    }
+}
